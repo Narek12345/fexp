9705e182731aca354fba7bbb57ece41df63d87e1
Narek12345 <narekbayanduryan16@gmail.com>
2023-07-23 00:07:55 +0300

    Adding the following fields to the model User: role, registration_time.

--- fexp/models.py
+++ fexp/models.py
@@ -1,14 +1,17 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from fexp import db
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer(), primary_key=True)
     
-    username = db.Column(db.String(255), index=True, nullable=True, unique=True)
-    password = db.Column(db.String(255), nullable=True)
+    username = db.Column(db.String(255), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registration_time = db.Column(db.DateTime, default=datetime.utcnow)
+    role = db.Column(db.String(10))
 
     def __repr__(self):
         return f'{self.id}:{self.username}'
@@ -24,10 +27,10 @@ class Employer(db.Model):
     __tablename__ = 'employer'
     id = db.Column(db.Integer(), primary_key=True)
     
-    first_name = db.Column(db.String(255), nullable=True)
-    last_name = db.Column(db.String(255), nullable=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
     phone_number = db.Column(db.String(255))
-    email = db.Column(db.String(255), nullable=True, unique=True)
+    email = db.Column(db.String(255), unique=True)
 
     user = db.Column(db.Integer, db.ForeignKey('user.id'))
     
@@ -39,10 +42,10 @@ class Student(db.Model):
     tablename = 'student'
     id = db.Column(db.Integer(), primary_key=True)
 
-    first_name = db.Column(db.String(), nullable=True)
-    last_name = db.Column(db.String(255), nullable=True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String(255))
     phone_number = db.Column(db.String(255))
-    email = db.Column(db.String(255), nullable=True, unique=True)
+    email = db.Column(db.String(255), unique=True)
 
     user = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
@@ -54,11 +57,11 @@ class JobVacansy(db.Model):
     __tablename__ = 'job_vacansy'
     id = db.Column(db.Integer(), primary_key=True)
 
-    title = db.Column(db.String(255), nullable=True)
-    salary = db.Column(db.Integer(), nullable=True)
+    title = db.Column(db.String(255))
+    salary = db.Column(db.Integer())
     description = db.Column(db.String(255))
-    experience = db.Column(db.String(255), nullable=True)
-    company = db.Column(db.String(255), nullable=True)
+    experience = db.Column(db.String(255))
+    company = db.Column(db.String(255))
     country = db.Column(db.String(255))
     city = db.Column(db.String(255))
     adress = db.Column(db.String(255))
@@ -73,15 +76,15 @@ class Summary(db.Model):
     tablename = 'summary'
     id = db.Column(db.Integer(), primary_key=True)
 
-    title = db.Column(db.String(255), nullable=True)
-    salary = db.Column(db.Integer(), nullable=False, default=None)
-    age = db.Column(db.Integer(), nullable=False, default=None)
-    experience = db.Column(db.String(255), nullable=True)
+    title = db.Column(db.String(255))
+    salary = db.Column(db.Integer(), default=None)
+    age = db.Column(db.Integer(), default=None)
+    experience = db.Column(db.String(255))
     country = db.Column(db.String(255))
     city = db.Column(db.String(255))
     adress = db.Column(db.String(255))
-    skills = db.Column()
-    biography = db.Column(db.String(1000), nullable=True)
+    skills = db.Column(db.String(150))
+    biography = db.Column(db.String(1000))
     
     student = db.Column(db.Integer(), db.ForeignKey('student.id'))
 
